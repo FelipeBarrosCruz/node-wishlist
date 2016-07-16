d5ef9e56da539fd0db53999025be4c3ba9daaace
FelipeBarrosCruz <felipe.barros.pt@gmail.com>
2016-07-16 05:40:32 -0300

    Add routes to wishlist components

--- env/development.js
+++ env/development.js
@@ -3,13 +3,11 @@
 module.exports = {
     env: 'development',
     application: {
-        port: 3000
+        port: 3000,
+        dir: __dirname.concat('/../app')
     },
     jwt: {
         secret: '5325b30a17bcfd8b1dfad051ba96174598b80b05',
-        unless: [
-            '/users/login',
-            '/users/status'
-        ]
+        unless: []
     }
 };
--- src/ApplicationServer.js
+++ src/ApplicationServer.js
@@ -5,14 +5,22 @@ let Express     = require('express'),
     ExpressJwt  = require('express-jwt');
 
 function ApplicationServer(configuration) {
-    let Application = Express(),
-        Errors      = ((list) => {
-            let response = {};
-            list.map((error) => {
-                response[error] = require(`./errors/${error}`);
-            })
-            return response;
-        })([401, 404]);
+    let Application = Express();
+
+    let JwtConfiguration = ExpressJwt({
+        secret: configuration.jwt.secret,
+        getToken: (req) => {
+            return req.headers.bearer || null;
+        }
+    }).unless(configuration.jwt.unless);
+
+    let Errors = ((list) => {
+        let response = {};
+        list.map((error) => {
+            response[error] = require(`./errors/${error}`);
+        })
+        return response;
+    })([401, 404]);
 
     Application.use(BodyParser.urlencoded({
         extended: false
@@ -22,16 +30,19 @@ function ApplicationServer(configuration) {
 
     Application.use(ExpressCors());
 
-    Application.use(ExpressJwt({
-        secret: configuration.jwt.secret,
-        getToken: (req) => {
-            return req.headers.bearer || null;
-        }
-    }).unless(configuration.jwt.unless));
+    let Routes = require(configuration.application.dir);
 
+    for(let route in Routes) {
+        let Module = Routes[route]({
+            name:   route,
+            jwt:    JwtConfiguration
+        });
+        Application.use(`/${route}`, Module);
+    }
+
+    Application.use(JwtConfiguration);
     Application.use(Errors['401']);
     Application.use(Errors['404']);
-
 
     if (configuration.env === 'development') {
         let Morgan = require('morgan');
