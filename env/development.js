8a117825774825a087afd54fff29e840bcb6b424
FelipeBarrosCruz <felipe.barros.pt@gmail.com>
2016-07-18 23:59:45 -0300

    Add env folder

---
+++ env/development.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+    env: 'development',
+    application: {
+        port: 3000,
+        dir: __dirname.concat('/../app')
+    },
+    jwt: {
+        secret: '5325b30a17bcfd8b1dfad051ba96174598b80b05',
+        unless: []
+    },
+    db: {
+        host: 'node-wishlist.cwer2upqmp5c.us-west-2.rds.amazonaws.com',
+        user: 'whishlist',
+        port: 5432,
+        password: '1q2w3e4r5t',
+        database: 'whishlist'
+    }
+};
