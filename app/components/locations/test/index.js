e9797f8a3bc1ab3e7b667398166a7a1d9baf741d
FelipeBarrosCruz <felipe.barros.pt@gmail.com>
2016-07-18 23:56:51 -0300

    Change test entry point of locations component to index

--- app/components/locations/test/index.js
+++ app/components/locations/test/index.js
@@ -2,13 +2,25 @@
 
 function Test(Repository) {
 
-    function loadUnitTest(name) {
-        return require(`./src/${name}`)(Repository)
-    }
+    var should = require('chai').should();
 
-    return {
-        searchByCep: loadUnitTest('search-by-cep')
-    };
+    describe('Testing locations component', function() {
+
+        beforeEach(function() {
+            this.locationHelper = Repository.get('app.helper.locations');
+            this.cep = '01311300';
+        });
+
+        it('Should search an address by cep', function (done) {
+          this.locationHelper.searchByCEP({
+                cep: this.cep
+            }, (err, status, result) => {
+                (err === null).should.equal(true);
+                status.should.equal(true);;
+                return done(err);
+            });
+        });
+    });
 }
 
 module.exports = Test;
